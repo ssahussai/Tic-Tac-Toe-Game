39b21306e3ca80ff7e55371e3727f3c228581638
Salma Khan <hussainsalma37@gmail.com>
2019-10-25 11:28:55 -0500

    created game structre and style in CSS HTML

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-document.getElementById('gameboard').addEventListener('click', handleClick);
-
-
-
+/*----- constants -----*/
 const GAMEBOARD = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
-
-
-
-init();
-
-function init() {
- 
-}
-
-
-function handleClick(event) {
-  console.log()
-}
-
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+  
+  /*----- app's state (variables) -----*/
+  /*----- cached element references -----*/
+  /*----- event listeners -----*/
+  document.getElementById('gameboard').addEventListener('click', handleClick);
+  
+  /*----- functions -----*/
+  init();
+  
+  function init() {
+   
+  }
+  
+  
+  function handleClick(event) {
+    console.log()
+  }
+  
+  
