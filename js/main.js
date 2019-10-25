431363a12bcdeb5f61de4b166815216f42b231e1
Salma Khan <hussainsalma37@gmail.com>
2019-10-25 10:45:51 -0500

    planning the project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.getElementById('gameboard').addEventListener('click', handleClick);
+
+
+
+const GAMEBOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
+
+
+init();
+
+function init() {
+ 
+}
+
+
+function handleClick(event) {
+  console.log()
+}
+
