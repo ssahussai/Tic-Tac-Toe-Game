88a163571afeac8fb4bf64549d6209cf73210bd5
Salma Khan <hussainsalma37@gmail.com>
2019-10-29 08:44:05 -0500

    finished working on js and CSS

--- js/main.js
+++ js/main.js
@@ -1,68 +1,76 @@
 /*----- constants -----*/
 const PLAYER = {
     '1': 'X',
-    '-1': 'O'
+    '-1': 'O',
+    'null': '' 
 }; 
 
+const possibleWinCombo = [ 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
   /*----- app's state (variables) -----*/
 let turn, winner, gameboard;
 
   /*----- cached element references -----*/
-  // We need to cache the element ref to message so we can display who's turn, or if it's a tie
-  const gameboardDOM = document.querySelectorAll('.square');
+  // We need to cache the 9 elements ref to message so we can display who's turn, or if it's a tie 
+  const squares = document.querySelectorAll('.square');
+  const message = document.getElementById('message');
 
-  /*
-  NodeList(9) 
-  [div.row1.square.textContent = PLAYER['1'], div.row1.square, div.row1.square, 
-    div.row2.square, div.row2.square, div.row2.square, 
-    div.row3.square, div.row3.square, div.row3.square]
-   */
 
   /*----- event listeners -----*/
-  document.getElementById('gameboard').addEventListener('click', handleClick); //already initialized and called the init function
-  
+  document.getElementById('gameboard').addEventListener('click', handleClick); //event listener for gameboard 
+  document.getElementById('reset').addEventListener('click', init); //best way to reset the game is calling the init() function
+
   /*----- functions -----*/
-  init(); //init gets the game ready to play
+  init(); //this makes the game to start as soon as the game loads. This initialize the stage and call render  
   
   
   function init() {
-      turn = PLAYER['1']; // this sets the first turn to 'X'
-      gameboard = [
-          [null, null, null],
-          [null, null, null],
-          [null, null, null]
-      ];
+      turn = 1 
+      gameboard = [ null, null, null, null, null, null, null, null, null ];
       winner = false;
       render();
-   //whose turn is it
-   //clear the board at the end of the game = reset
-   //call render again and transfer those game to DOM 
   }
   
-  
-  function handleClick(event) {
-    // We need to identify which square was clicked
-    // we need to map that same position to the gameboard array in javascript
-    // we need to check who's turn it is
-    // ....i.e. change the null value in the array to 1 or -1 depending on who's turn
-    // we need to to call render
+  //handleClick function will tell you what to happen after user clicks the box 
+  function handleClick(evt) {
+    const boxSelected = parseInt(evt.target.dataset.index); //selecting boxes 
+    // if(winner || gameboard[boxSelected]) return;
+    gameboard[boxSelected] = turn;
+    turn *= -1
+    winner = winningLogic();
+    render();
+    }
+
+  function winningLogic() {
+      for (let i = 0; i < possibleWinCombo.length; i++) {
+        if(Math.abs(gameboard[possibleWinCombo[i][0]] + 
+                    gameboard[possibleWinCombo[i][1]] + 
+                    gameboard[possibleWinCombo[i][2]]) === 3) return gameboard[possibleWinCombo[i][0]];
+      } if (gameboard.includes(null)) return false;
+      return 'T';
   }
 
-  function checkWinner() {
-      // Check the gameboard array for the following sequences:
-      // 00, 01, 02
-      // 00, 01, 02
-      // Math.abs 
-  }
 
-  function render() {
-    // render needs to loop through the gameboard to check for 1 or -1 or null
-    // if it's 1, change textContent to X, we can map to our loopup object
-    // if it's -1, change textContent to O
-    // if it's null, change textContent = ""
-    // We need to check for winner - call another function
-    // We need to switch turns after the play has been made
-    // we'll need to change the textCOntent of message to the turn or ties
-  }
+    function render() {
+      for (let i = 0; i < gameboard.length; i++){
+        squares[i].textContent = PLAYER[gameboard[i]];
+      };
+      if (!winner) {
+        message.textContent = `Now it's ${PLAYER[turn]}'s turn'`;
+      } else if (winner === 'T') {
+        message.textContent = 'This game is a TIE';
+      } else {
+        message.textContent = `Congratulations! ${PLAYER[winner]} won!`
+      }
+    } 
+
   
   
